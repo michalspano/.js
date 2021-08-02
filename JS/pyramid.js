7455e8dbf90880b9637ddb095e39ef6584e3b4d6
michalspano <speko.spano@gmail.com>
2021-08-02 21:04:03 +0200

    Utilise pyramid.js

--- JS/pyramid.js
+++ JS/pyramid.js
@@ -14,39 +14,9 @@ function createPyramid(height)
     }
 }
 
-// Create a spiral terminal effect
-function spiral(height) 
-{
-    // Create constants for available symbols to choose from
-    const symbol1 = '[', symbol2 = ']';
-    function enumerate(par) 
-    {
-        // Return symbol1 is i-th instance is divisible by 2
-        if (par % 2 == 0)
-        {
-            return symbol1;
-        }
-        else 
-        {
-            // Otherwise return the other symbol
-            return symbol2;
-        }
-    }
-
-    // Iterate with the boundary of predefined height
-    for (let i = 1; i <= height; i++)
-    {
-        // Use returned value from the enumerate function
-        var symbol = enumerate(i);
-        // Print out the output
-        console.log(space.repeat(height - i) + symbol.repeat(i) + symbol + symbol.repeat(height - i));
-    }
-}
-
 // Declare constant char identifiers 
 const space = ' ', hash = '#';
 const MAX = 10;
 
-// Invoke functions
-// createPyramid(MAX);
-spiral(MAX);
+// Invoke the pyramid function
+createPyramid(MAX);
