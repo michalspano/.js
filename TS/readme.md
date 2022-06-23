d54d72ae447eecc792a09b4dac297a4899a983ba
michalspano <speko.spano@gmail.com>
2022-06-23 13:04:26 +0200

    Add `readme.md`; remove redundant `.js` files from `TS/`

---
+++ TS/readme.md
@@ -0,0 +1,3 @@
+### TypeScript (`*.ts` source directory)
+___
+[ts-node](https://www.npmjs.com/package/ts-node); just like __node.js__ ~ *'TypeScript execution and REPL for node.js, with source map and native ESM support.'*
--- TS/run.sh
+++
@@ -1,8 +0,0 @@
-# TypeScript -> JavaScript
-# Automated script to compile and run the test suite
-# This is just a wrapper around the node command and is file specific
-# serves the role of an examplary script
-function run() {
-    tsc index.ts && node index.js
-}
-run
